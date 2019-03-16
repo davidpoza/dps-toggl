9e94cf10cdf13223e1e24317fb7b54018c1b2238
David Poza <dpsuarez@triskelsoft.com>
2019-03-16 17:41:56 +0100

    merge fix

--- src/reducers/initialState.js
+++ src/reducers/initialState.js
@@ -3,11 +3,7 @@ const initialState = {
         name: null,
         email: null,
         token: null,
-<<<<<<< HEAD
         loading: false,
-=======
-        login_in: false,
->>>>>>> 6708c0f19cffc169ffbb4952f1f34662e8e16118
         error: null
     },
     interface: {
