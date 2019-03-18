e1a5bf6941ab0624ab393240aa3830f65f3054d3
David Poza Suarez <pozasuarez@gmail.com>
2019-03-18 14:08:21 +0100

    test Link from react-router-dom

--- src/components/LoggedInfoComponent/LoggedInfoComponent.js
+++ src/components/LoggedInfoComponent/LoggedInfoComponent.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react'
+import {Link} from 'react-router-dom';
+
 import styles from './LoggedInfoComponent.scss';
 import avatar from '../../images/avatar_sample.jpg'
 
@@ -9,21 +11,20 @@ class LoggedInfoComponent extends Component{
     render(){
         return(
             <div className={"d-flex justify-content-between " + styles.box}> 
-                     <div className={styles.username}>
-                        <p className="m-0">{this.props.username}</p>
-                        <p className="m-0">Logout</p>
-                     </div>
-                     { this.props.expanded ? 
-                        <img className={styles.avatar} src={avatar} href="#" />   
-                       :
-                        <img className={styles.avatar} src={avatar} data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" />                    
-                     }
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">Perfil</a>
-                        <a class="dropdown-item" href="#">Logout</a>
-                    </div>
-                     
-                     
+                 <div className={styles.username}>
+                    <p className="m-0">{this.props.username}</p>
+                    <p className="m-0"><Link to="/login">Login</Link></p>
+                    <p className="m-0">Logout</p>
+                 </div>
+                 { this.props.expanded ? 
+                    <img className={styles.avatar} src={avatar} href="#" />   
+                   :
+                    <img className={styles.avatar} src={avatar} data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" />                    
+                 }
+                <div className="dropdown-menu">
+                    <a className="dropdown-item" href="#">Perfil</a>
+                    <a className="dropdown-item" href="#">Logout</a>
+                </div>
             </div>
         )
     }
