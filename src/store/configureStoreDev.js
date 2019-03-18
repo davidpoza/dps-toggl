6e893d045ef8173a948046d105b410de67c52111
David Poza Suarez <pozasuarez@gmail.com>
2019-03-18 14:04:24 +0100

    thunk lib added for async action creators

--- src/store/configureStoreDev.js
+++ src/store/configureStoreDev.js
@@ -1,12 +1,12 @@
 import {createStore, applyMiddleware} from 'redux';
 import { createLogger } from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension';
-
+import thunk from 'redux-thunk';
 
 import rootReducer from '../reducers';
 
 const enhancer = composeWithDevTools(
-    applyMiddleware(createLogger()) 
+    applyMiddleware(thunk, createLogger()) 
 );
 
 export default function configureStore(initialState){
