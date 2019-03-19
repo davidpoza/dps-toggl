fa71ed1e331ad94247a25d7dd522932fc2217f31
David Poza Suarez <pozasuarez@gmail.com>
2019-03-19 15:01:27 +0100

    loading component created

--- src/components/LoggedInfoComponent/LoggedInfoComponent.js
+++ src/components/LoggedInfoComponent/LoggedInfoComponent.js
@@ -11,10 +11,10 @@ class LoggedInfoComponent extends Component{
     render(){
         return(
             <div className={"d-flex justify-content-between " + styles.box}> 
-                 <div className={styles.username}>
+                 <div>
                     {this.props.user.token != null ? 
                         (   
-                            <div>
+                            <div className={styles.username}>
                                 <p className="m-0">{this.props.user.first_name} {this.props.user.last_name}</p>
                                 <p className="m-0"><span className={styles.link} onClick={this.props.actions.logoutUser}>Logout</span></p>
                             </div>
@@ -32,14 +32,14 @@ class LoggedInfoComponent extends Component{
 
                     this.props.expanded ?
 
-                    <img className={styles.avatar} src={this.props.user.avatar} href="#" />   
+                    <img className={styles.avatar} src={this.props.user.avatar} data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"/>   
                    :
                     <img className={styles.avatar} src={this.props.user.avatar} data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" />                    
                  
                 }
                 <div className="dropdown-menu">
                     <a className="dropdown-item" href="#">Perfil</a>
-                    <a className="dropdown-item" className={styles.link} onClick={this.props.actions.logoutUser}>Logout</a>
+                    <a className="dropdown-item" onClick={this.props.actions.logoutUser}>Logout</a>
                 </div>
             </div>
         )
