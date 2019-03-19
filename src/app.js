01a20edb8002fb61a48b1eeb5f8226973581c6e3
David Poza Suarez <pozasuarez@gmail.com>
2019-03-19 18:15:19 +0100

    link to local libs jquery and bootstrap

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
+import './imports.scss'; //provocamos que webpack procese el archivo con un filter que lo concatenará al style.css
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {HashRouter} from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 
-import 'bootstrap'; //importamos los js de bootstrap
-import './imports.scss'; //provocamos que webpack procese el archivo con un filter que lo concatenará al style.css
 import AppComponent from './components/AppComponent/AppComponent';
 import configureStore from './store/configureStore';
 import './store/configureStore';
