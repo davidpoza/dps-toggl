ad7e6d7171f40fc8dadbd72c3d659f8a9a7921a5
David Poza Suarez <pozasuarez@gmail.com>
2019-03-19 17:07:00 +0100

    fix production webpack config

--- src/store/configureStoreProd.js
+++ src/store/configureStoreProd.js
@@ -1,8 +1,12 @@
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
 
 import rootReducer from '../reducers';
 
+const enhancer = compose(
+    applyMiddleware(thunk)
+);
 
 export default function configureStore(initialState){
-    return createStore(rootReducer, initialState);
+    return createStore(rootReducer, initialState, enhancer);
 }
