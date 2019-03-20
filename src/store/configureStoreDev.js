b93c55d0c9c8b9520ddfe324c2f33d41c21367d7
David Poza Suarez <pozasuarez@gmail.com>
2019-03-20 17:36:08 +0100

    using redux-localstorage-simple i save the redux state in localstorage

--- src/store/configureStoreDev.js
+++ src/store/configureStoreDev.js
@@ -2,13 +2,14 @@ import {createStore, applyMiddleware} from 'redux';
 import { createLogger } from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
+import { save, load } from "redux-localstorage-simple"
 
 import rootReducer from '../reducers';
 
 const enhancer = composeWithDevTools(
-    applyMiddleware(thunk, createLogger()) 
+    applyMiddleware(thunk, save(), createLogger()) 
 );
 
 export default function configureStore(initialState){
-    return createStore(rootReducer, initialState, enhancer);
+    return createStore(rootReducer, load({preloadedState:initialState }), enhancer);
 }
