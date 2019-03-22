5217d14b0e3acfe37df2baab499129dd96dd03e5
David Poza Suarez <pozasuarez@gmail.com>
2019-03-22 18:14:35 +0100

    added more date functions to utils module

--- src/utils/index.js
+++ src/utils/index.js
@@ -20,6 +20,26 @@ const utils = {
 
     standarizeDate(date){
         return `${date.getFullYear()}-${this.pad(date.getMonth(),2)}-${this.pad(date.getDay(),2)} ${this.pad(date.getHours(),2)}:${this.pad(date.getMinutes(),2)}:${this.pad(date.getSeconds(),2)}`;
+    },
+
+    getHour(date){
+        console.log(date);
+        let regex = /(\d{2}:\d{2}):\d{2}/;
+        return date.match(regex)[1];
+    },
+
+    //two dates are of the same day
+    diffHoursBetDates(date_start, date_end){
+        let regexHour = /(\d{2}):\d{2}:\d{2}/;
+        let regexMin = /\d{2}:(\d{2}):\d{2}/;
+        let regexSec = /\d{2}:\d{2}:(\d{2})/;
+        let hour1 = date_start.match(regexHour)[1];
+        let hour2 = date_end.match(regexHour)[1];
+        let min1 = date_start.match(regexMin)[1];
+        let min2 = date_end.match(regexMin)[1];
+        let sec1 = date_start.match(regexSec)[1];
+        let sec2 = date_end.match(regexSec)[1];
+        return(this.pad(parseInt(hour2-hour1),2)+":"+this.pad(parseInt(min2-min1),2)+":"+this.pad(parseInt(sec2-sec1),2));
     }
 }
 
