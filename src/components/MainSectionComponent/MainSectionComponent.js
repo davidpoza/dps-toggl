b3faef6e234426e6a8ef9fea7718bae7735282da
David Poza Suarez <pozasuarez@gmail.com>
2019-03-28 11:54:29 +0100

    added toast component for displaying errors. Using bootstrap toast

--- src/components/MainSectionComponent/MainSectionComponent.js
+++ src/components/MainSectionComponent/MainSectionComponent.js
@@ -1,21 +1,41 @@
 import React, {Component} from 'react'
 import {Route} from 'react-router-dom';
 
-
 import styles from './MainSectionComponent.scss';
 import LoginContainer from '../LoginComponent/LoginContainer';
 import PrivateRoute from '../PrivateRouteComponent/PrivateRouteComponent';
 import TimerDashboardContainer from '../TimerDashboardComponent/TimerDashboardContainer';
 import ConfigComponent from '../ConfigComponent/ConfigComponent';
+import ToastComponent from '../ToastComponent/ToastComponent';
 
 
 class MainSectionComponent extends Component{
     constructor(props){
         super(props);
     }
+
+    componentDidMount(){
+       // 
+    }
+
+    componentDidUpdate(prevProps) {
+        if (!prevProps.task.error.message && this.props.task.error.message)
+            $('#toast1').toast("show");
+        if (!prevProps.user.error.message && this.props.user.error.message)
+            $('#toast2').toast("show");
+        if (!prevProps.project.error.message && this.props.project.error.message)
+            $('#toast3').toast("show");
+    }
+
+
     render(){
         return(
             <div className="h-100">
+                <div className={styles.toasts}>
+                    <ToastComponent id="toast1" message={this.props.task.error.message} />
+                    <ToastComponent id="toast2" message={this.props.user.error.message} /> 
+                    <ToastComponent id="toast3" message={this.props.project.error.message} />
+                </div>                 
                 <PrivateRoute exact path="/" component={TimerDashboardContainer} />
                 <PrivateRoute exact path="/config" component={ConfigComponent} />
                 <Route path="/login" component={LoginContainer} />
