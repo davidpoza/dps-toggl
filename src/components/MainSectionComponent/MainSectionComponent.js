1996a7637858cede27779af937553915694d13ca
David Poza Suarez <pozasuarez@gmail.com>
2019-03-28 12:30:32 +0100

    stackable toasts

--- src/components/MainSectionComponent/MainSectionComponent.js
+++ src/components/MainSectionComponent/MainSectionComponent.js
@@ -31,14 +31,17 @@ class MainSectionComponent extends Component{
     render(){
         return(
             <div className="h-100">
-                <div className={styles.toasts}>
-                    <ToastComponent id="toast1" message={this.props.task.error.message} />
-                    <ToastComponent id="toast2" message={this.props.user.error.message} /> 
-                    <ToastComponent id="toast3" message={this.props.project.error.message} />
-                </div>                 
+                
                 <PrivateRoute exact path="/" component={TimerDashboardContainer} />
                 <PrivateRoute exact path="/config" component={ConfigComponent} />
                 <Route path="/login" component={LoginContainer} />
+
+                    <div className={styles.toasts}>
+                        <ToastComponent id="toast1" message={this.props.task.error.message} />
+                        <ToastComponent id="toast2" message={this.props.user.error.message} /> 
+                        <ToastComponent id="toast3" message={this.props.project.error.message} />
+                    </div>                 
+
             </div>
         )
     }
