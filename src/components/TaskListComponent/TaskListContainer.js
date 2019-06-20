27c9ff11fcc73b1be4904085c078c9eb42c65132
David Poza Suarez <pozasuarez@gmail.com>
2019-06-20 12:29:24 +0200

    compound tasks adapted to new API

--- src/components/TaskListComponent/TaskListContainer.js
+++ src/components/TaskListComponent/TaskListContainer.js
@@ -20,7 +20,7 @@ class TaskListContainer extends Component{
     }
 
     render(){
-       
+
         return(
             <TaskListComponent
             token={this.props.token}
@@ -36,32 +36,32 @@ function mapStateToProps (state, props) {
     tasks = tasks.sort((a,b)=>{
       let task_a = state.taskReducer.tasks_entities[a];
       let task_b = state.taskReducer.tasks_entities[b];
-      return task_b.start_hour.localeCompare(task_a.start_hour);    
+      return task_b.start_hour.localeCompare(task_a.start_hour);
     });
     //aqui tengo que componer las tareas con igual desc e igual proyecto
     //los que tienen parent:-1 son padres, todos son padres inicialmente
 
-    tasks = tasks.map(t=>({id:t, parent:-1}));
-    
+    tasks = tasks.map(t=>({_id:t, parent:-1}));
+
     for(let i=0; i<tasks.length; i++){
       for(let j=i+1; j<tasks.length; j++){
-        let task_a = state.taskReducer.tasks_entities[tasks[i].id];
-        let task_b = state.taskReducer.tasks_entities[tasks[j].id];
-        if(tasks[i].parent == -1 && task_a.desc == task_b.desc && 
+        let task_a = state.taskReducer.tasks_entities[tasks[i]._id];
+        let task_b = state.taskReducer.tasks_entities[tasks[j]._id];
+        if(tasks[i].parent == -1 && task_a.desc == task_b.desc &&
           task_a.project == task_b.project ){
-          tasks[j].parent = task_a.id;
+          tasks[j].parent = task_a._id;
         }
       }
     }
 
-    let compound_tasks = tasks.map(t=>({id:t.id, parent:t.parent, children:tasks.filter(task=>task.parent == t.id)}));
-    
+    let compound_tasks = tasks.map(t=>({id:t._id, parent:t.parent, children:tasks.filter(task=>task.parent == t._id)}));
+
     return {
-      token: state.userReducer.token,      
+      token: state.userReducer.token,
       tasks: compound_tasks,
     }
   }
-  
+
   function mapDispatchToProps (dispatch) {
     return {
       userActions: bindActionCreators(userActions, dispatch),
@@ -70,5 +70,5 @@ function mapStateToProps (state, props) {
       tagActions: bindActionCreators(tagActions, dispatch),
     }
   }
-  
+
 export default connect(mapStateToProps, mapDispatchToProps)(TaskListContainer);
