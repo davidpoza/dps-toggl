abbcd2fb48454510ac2ab6eec5b4cc6eca3b1d59
David Poza Suarez <pozasuarez@gmail.com>
2019-06-27 11:24:38 +0200

    removed add btn from UserSectionComponent

--- src/components/UserSectionComponent/UserSectionComponent.js
+++ src/components/UserSectionComponent/UserSectionComponent.js
@@ -60,7 +60,6 @@ class UserSectionComponent extends Component{
             <div className={"d-flex flex-column justify-content-start h-100"}>
                 <div className={"d-flex justify-content-between "+styles.header}>
                     <h1>{lang[config.lang].user_section_title}</h1>
-                    <button className="btn-lg btn-primary" data-toggle="modal" data-target="#projectCreateModal" onClick={this.handleOpenModal}><i className="fas fa-plus-circle"></i></button>
                 </div>
                 <div className={"flex-grow-1 " + styles.projectlist}>
                     <UserListComponent
@@ -73,42 +72,7 @@ class UserSectionComponent extends Component{
                     />
                 </div>
 
-                <div className="modal fade" id="projectCreateModal" ref={this.modal} tabIndex="-1" role="dialog" aria-labelledby="createProjectLabel" aria-hidden="true" onKeyPress={this.handleOnKeyPress}>
-                <div className="modal-dialog modal-lg" role="document">
-                    <div className="modal-content">
-                    <div className="modal-header">
-                        <h5 className="modal-title" id="createProjectLabel">{lang[config.lang].btn_new_project}</h5>
-                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div className={"modal-body "+styles.modal_body}>
-                        <input className={styles.input} ref={this.projectNameInput} placeholder={lang[config.lang].project_name_placeholder}></input>
-                        <div className="dropdown">
-                            <button className={"btn dropdown-toggle "+styles.dropdown_btn} type="button" id="dropdownProjectColorButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i className={"fas fa-square "+styles.color} style={{color: this.state.colorPicker}}></i>
-                            </button>
-                            <div className={"dropdown-menu "+styles.color_dropdown} aria-labelledby="dropdownProjectColorButton">
-                                <div className="container">
-                                    <div className="row justify-content-center">
-                                    {
-                                        config.project_colors.map((e,index)=>(
-                                            <i key={"color"+index} className={"fas fa-square "+styles.color} style={{color: e}} onClick={this.handleColorPick}></i>
-                                        ))
-                                    }
 
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-                    <div className="modal-footer">
-                        <button type="button" className="btn btn-primary" onClick={this.handleCreateProject}>{lang[config.lang].btn_save_changes}</button>
-                    </div>
-                    </div>
-                </div>
-                </div>
                 <LoadingComponent isLoading={this.props.user_loading||this.props.project_loading} />
             </div>
         )
