bd5972759d5c37c482122bcca17e65018e86043c
David Poza Suarez <pozasuarez@gmail.com>
2019-06-27 18:35:17 +0200

    title for save/delete buttons on project/user detail views

--- src/components/ProfileSectionComponent/ProfileSectionComponent.js
+++ src/components/ProfileSectionComponent/ProfileSectionComponent.js
@@ -88,8 +88,8 @@ class ProfileSectionComponent extends Component{
                     <div className={"d-flex justify-content-between "+styles.header}>
                         <h1>{lang[config.lang].profile_section_title}</h1>
                         <div>
-                            <button className="btn btn-danger p-3 mr-2" data-toggle="modal" data-target="#deleteModal"><i className="fas fa-trash"></i></button>
-                            <button className="btn btn-primary p-3" onClick={this.handleSaveProfile}><i className="fas fa-save"></i></button>
+                            <button className="btn btn-danger p-3 mr-2" data-toggle="modal" data-target="#deleteModal" title={lang[config.lang].btn_title_delete_user}><i className="fas fa-trash"></i></button>
+                            <button className="btn btn-primary p-3" onClick={this.handleSaveProfile}><i className="fas fa-save" title={lang[config.lang].btn_title_save_user}></i></button>
                         </div>
                     </div>
                     <div className={"flex-grow-1 " + styles.profile}>
