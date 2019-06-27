13aa3e2519b50017e9aeaca070e7e0719941932c
David Poza Suarez <pozasuarez@gmail.com>
2019-06-27 11:20:30 +0200

    now unactive users and admin are highlighted

--- src/components/UserComponent/UserComponent.js
+++ src/components/UserComponent/UserComponent.js
@@ -19,7 +19,7 @@ class UserComponent extends Component{
 
     render(){
         return(
-            <li className={"row m-0 m-lg-1 justify-content-between " + styles.user } onClick={this.handleOnClick.bind(this,this.props.user._id)}>
+            <li className={"row m-0 m-lg-1 justify-content-between " + styles.user } style={(this.props.user.active==true) ? {} : {opacity: 0.5}} onClick={this.handleOnClick.bind(this,this.props.user._id)}>
                 <div className={"col-1 p-0"} >
                     <img className={styles.avatar} src={config.api_url+"/users/avatar/"+this.props.user.avatar}/>
                 </div>
@@ -28,7 +28,8 @@ class UserComponent extends Component{
                     {this.props.user.email}
                 </div>:
                 <div className={"col-4"} >
-                    {this.props.user.email}
+                    {(this.props.user.admin==true) ? <i className="fas fa-star"></i> : "" }
+                    {(this.props.user.admin==true) ? " "+this.props.user.email:this.props.user.email}
                 </div>
                 }
                 { !utils.isMobile() &&
