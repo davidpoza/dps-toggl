4970c73c14c42b6875008e666c5d2c72978c97ee
David Poza <dpsuarez@triskelsoft.com>
2019-07-12 14:59:26 +0200

    fixed on production I'm not composing redux enhancer with save() middleware from redux-localstorage-simple closes #169

--- src/store/configureStoreProd.js
+++ src/store/configureStoreProd.js
@@ -1,12 +1,13 @@
 import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
+import { save, load } from "redux-localstorage-simple"
 
 import rootReducer from '../reducers';
 
 const enhancer = compose(
-    applyMiddleware(thunk)
+    applyMiddleware(thunk, save())
 );
 
 export default function configureStore(initialState){
-    return createStore(rootReducer, initialState, enhancer);
+    return createStore(rootReducer, load({preloadedState:initialState }), enhancer);
 }
