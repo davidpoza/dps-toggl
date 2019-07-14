ff3a2dfa3b95c4a2b63b7150e22b420922f87c8b
David Poza <dpsuarez@triskelsoft.com>
2019-07-14 12:28:47 +0200

    fixed production url

--- src/config/config.js
+++ src/config/config.js
@@ -5,7 +5,7 @@ const config = {
         "#09a9f4", "#c87bf6", "#eb548d", "#fa8e49", "#c67639", "#51c93d", "#33bb9b", "#e19a86", "#3853b5", "#a354a6", "#f1c451", "#1f5615", "#89211f", "#e23c39", "#000000"
        ],
     records_per_page: 7,
-    api_url: process.env.NODE_ENV === "production"? "https://dpstogglapi1.davidinformatico.com/_" :
+    api_url: process.env.NODE_ENV === "production"? "https://toggl.davidinformatico.com/api" :
                                                     "http://localhost:3000/api"
 };
 
